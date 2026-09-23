b864103322fd72ea7c51aa1aa73e63d151f34a28
VeoQeo <qensyteam@gmail.com>
2026-09-23 18:19:53 +0300

    feat(recipes): gcc builds C++ exceptions and ships libstdc++
    The recipe now matches how the package is actually built: gettext moved
    aside, the endianness answers given to configure, init_array forced on,
    the target option file for -pthread, crtbegin/crtend and libgcc copied in,
    and libstdc++ with its headers so g++ links C++ programs on Cervus.
    
    Signed-off-by: VeoQeo <qensyteam@gmail.com>

--- recipes/gcc/cervus.h
+++ recipes/gcc/cervus.h
@@ -1,20 +1,20 @@
-/* Cervus OS target for GCC (x86_64-cervus).
-   Static, non-PIE, no dynamic loader; links Cervus's crt0 + libcervus + libgcc. */
-
 #undef TARGET_CERVUS
 #define TARGET_CERVUS 1
 
 #undef LIB_SPEC
-#define LIB_SPEC "--start-group -lcervus -lgcc --end-group"
+#define LIB_SPEC "%{!shared:--start-group %{pie:-lcervus_pic;:-lcervus} -lgcc --end-group}"
 
 #undef STARTFILE_SPEC
-#define STARTFILE_SPEC "crt0.o%s"
+#define STARTFILE_SPEC "%{!shared:crt0.o%s} %{shared|pie:crtbeginS.o%s;:crtbegin.o%s}"
 
 #undef ENDFILE_SPEC
-#define ENDFILE_SPEC ""
+#define ENDFILE_SPEC "%{shared|pie:crtendS.o%s;:crtend.o%s}"
 
 #undef LINK_SPEC
-#define LINK_SPEC "-static %{!Ttext-segment*:%{!Wl,-Ttext-segment*:-Ttext-segment=0x401000}} %{shared:-shared}"
+#define LINK_SPEC "%{shared:-shared --hash-style=sysv;pie:--export-dynamic --hash-style=sysv -dynamic-linker /lib/ld-cervus.elf;:-static %{!Ttext-segment*:%{!Wl,-Ttext-segment*:-Ttext-segment=0x401000}}}"
+
+#undef TARGET_ASM_FILE_END
+#define TARGET_ASM_FILE_END file_end_indicate_exec_stack
 
 #undef DRIVER_SELF_SPECS
 #define DRIVER_SELF_SPECS "%{!mred-zone:%{!mno-red-zone:-mno-red-zone}}"
